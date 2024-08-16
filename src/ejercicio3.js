3bb2252756cd9f14c6d326591a13563fa9dd055d
Michelle-0793 <mlopez@fwdcostarica.com>
2024-08-16 11:34:07 -0600

    ejercicio 3

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,5 +1,34 @@
-// Inserte el código aquí
+//Función para clonar arreglo
+function clonarArreglo(arreglo) {
+    return [...arreglo]   
+}
 
+const arreglo = [1, 2, 3, 4];
+let arregloClonado = clonarArreglo(arreglo);
+
+////Función para clona objeto
+function clonarObjeto(jardin) {
+    return {...jardin}   
+}
+
+const jardin = {
+    flores: "rosas",
+    arboles: "almendro",
+    macetas: 3,
+};
+let jardinClonado = clonarObjeto(jardin);
+
+//Función para sumar varios números
+function sumar(...numeros) {
+    return numeros.reduce ((acumulador, numeroActual) => acumulador + numeroActual, 0)
+}
+
+const resultadoSuma = sumar(2,4,5,6);
+
+//Función saludar
+function saludar(nombre = `Desconocido`) {
+    return `Hola`, nombre;    
+}
 
 
 // NO MODIFICAR
